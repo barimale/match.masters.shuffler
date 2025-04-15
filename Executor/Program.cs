6445b2e0a0e6e5f663b0e26d7bb863f670715d21
Mateusz <mateusz.wolnica@gmail.com>
2025-04-15 18:46:31 +0200

    .

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -2,8 +2,8 @@
 using Discrete.Random.Matrix.Generator.Utilities;
 
 //given
-var rowsAmount = 10;
-var columnsAmount = 10;
+var rowsAmount = 30;
+var columnsAmount = 30;
 var lower = 0;
 var upper = 5;
 var isStrict = true;
