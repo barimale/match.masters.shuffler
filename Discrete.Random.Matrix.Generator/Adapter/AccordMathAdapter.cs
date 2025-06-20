9d9e523c5516833150b10e7a1caa5e0b9747c2de
Mateusz <mateusz.wolnica@gmail.com>
2025-06-20 19:25:03 +0200

    Renamed engine to adaptee.

--- Discrete.Random.Matrix.Generator/Adapter/AccordMathAdapter.cs
+++ Discrete.Random.Matrix.Generator/Adapter/AccordMathAdapter.cs
@@ -5,11 +5,11 @@ namespace Discrete.Random.Matrix.Generator.Adapter
     internal class AccordMathAdapter: INamedAdapter
     {
         private const string NAME = "Accord.Math";
-        private readonly IDiscreteUniformDistribution _engine;
+        private readonly IDiscreteUniformDistribution _adaptee;
 
         public AccordMathAdapter()
         {
-            _engine = new DiscreteUniformAccordMath();
+            _adaptee = new DiscreteUniformAccordMath();
         }
 
         public string GetUniqueName()
@@ -19,12 +19,12 @@ namespace Discrete.Random.Matrix.Generator.Adapter
 
         public Task<Tuple<double, double, string, int[]>> ExecuteCategoricalAsync(int lower, int upper, int n, int m, double[] masses, bool isStrict)
         {
-            return _engine.ExecuteCategoricalAsync(lower, upper, n, m, masses,isStrict);
+            return _adaptee.ExecuteCategoricalAsync(lower, upper, n, m, masses,isStrict);
         }
 
         public Task<Tuple<double, double, string, int[]>> ExecuteUniformAsync(int lower, int upper, int n, int m, bool isStrict)
         {
-            return _engine.ExecuteUniformAsync(lower, upper, n, m, isStrict);
+            return _adaptee.ExecuteUniformAsync(lower, upper, n, m, isStrict);
         }
     }
 }
--- Discrete.Random.Matrix.Generator/Adapter/MathNETAdapter.cs
+++ Discrete.Random.Matrix.Generator/Adapter/MathNETAdapter.cs
@@ -5,11 +5,11 @@ namespace Discrete.Random.Matrix.Generator.Adapter
     internal class MathNETAdapter : INamedAdapter
     {
         private const string NAME = "MathNet.Numerics";
-        private readonly IDiscreteUniformDistribution _engine;
+        private readonly IDiscreteUniformDistribution _adaptee;
 
         public MathNETAdapter()
         {
-            _engine = new DiscreteUniformDistributionMathNet();
+            _adaptee = new DiscreteUniformDistributionMathNet();
         }
 
         public string GetUniqueName()
@@ -19,12 +19,12 @@ namespace Discrete.Random.Matrix.Generator.Adapter
 
         public async Task<Tuple<double, double, string, int[]>> ExecuteCategoricalAsync(int lower, int upper, int n, int m, double[] masses, bool isStrict)
         {
-            return await _engine.ExecuteCategoricalAsync(lower, upper, n, m, masses,isStrict);
+            return await _adaptee.ExecuteCategoricalAsync(lower, upper, n, m, masses,isStrict);
         }
 
         public async Task<Tuple<double, double, string, int[]>> ExecuteUniformAsync(int lower, int upper, int n, int m, bool isStrict)
         {
-            return await _engine.ExecuteUniformAsync(lower, upper, n, m, isStrict);
+            return await _adaptee.ExecuteUniformAsync(lower, upper, n, m, isStrict);
         }
     }
 }
